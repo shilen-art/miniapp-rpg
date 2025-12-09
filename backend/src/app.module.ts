781d0cf4d428692708f2d73f5cf245a0aaa49b6c
Ivan Ivanov <ex3dex@MacBook-Pro-Ivan.local>
2025-12-09 13:14:52 +0100

    feat: fix Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
